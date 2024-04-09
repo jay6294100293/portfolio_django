dc49e5f480ae198630d0a35bee6704f1f113f76c
mrityunjay <mrityunjay.100293@gmail.com>
2024-04-09 03:30:27 -0700

    vercel change

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
-
-echo "BUILD START"
-
-# Create and activate virtual environment
-python3.8 -m venv venv
-source venv/bin/activate
-
-# Install or upgrade pip
-python -m pip install --upgrade pip
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Collect static files without prompting
-python manage.py collectstatic --no-input
-
-echo "BUILD END"
+##!/bin/bash
+#
+#echo "BUILD START"
+#
+## Create and activate virtual environment
+#python3.8 -m venv venv
+#source venv/bin/activate
+#
+## Install or upgrade pip
+#python -m pip install --upgrade pip
+#
+## Install dependencies
+#pip install -r requirements.txt
+#
+## Collect static files without prompting
+#python manage.py collectstatic --no-input
+#
+#echo "BUILD END"
