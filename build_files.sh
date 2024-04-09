08ad96a5d86c9e05f6e6a779a01bbc2db1557a63
mrityunjay <mrityunjay.100293@gmail.com>
2024-04-09 15:04:48 -0700

    Settings and vercel changed

--- build_files.sh
+++ build_files.sh
@@ -1,23 +1,4 @@
-##!/bin/bash
-#
-#echo "BUILD START"
-#
-## Create and activate virtual environment
-#python3.8 -m venv venv
-#source venv/bin/activate
-#
-## Install or upgrade pip
-#python -m pip install --upgrade pip
-#
-## Install dependencies
-#pip install -r requirements.txt
-#
-## Collect static files without prompting
-#python manage.py collectstatic --no-input
-#
-#echo "BUILD END"
-
-echo "BUILD START"
- python3.8 -m pip install -r requirements.txt
- python3.8 manage.py collectstatic --noinput --clear
+ echo "BUILD START"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -14,9 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Also Make aure To set allowed_hosts to '*'
 
@@ -123,8 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
